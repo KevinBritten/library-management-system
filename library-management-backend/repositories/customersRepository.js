2b8a17300f92677f6acd5333118c99332c126d45
Kevin Britten <kevinjbritten@gmail.com>
2025-01-23 15:14:32 -0500

    add customersRepository

---
+++ library-management-backend/repositories/customersRepository.js
@@ -0,0 +1,37 @@
+import MockDatabase from "../database/MockDatabase.js";
+
+class CustomersRepository {
+  constructor() {
+    this.db = MockDatabase;
+  }
+
+  create(customer) {
+    this.db.push(customer);
+  }
+
+  findAll() {
+    return this.db.customers;
+  }
+
+  findById(id) {
+    return this.db.customers.find((customer) => customer.id == id) || null;
+  }
+
+  findAllByName(name) {
+    return this.db.customers.map((customer) => customer.name == name);
+  }
+
+  update(id, updateFields) {
+    const customer = this.findById(id);
+    return customer ? Object.assign(customer, updateFields) : null;
+  }
+
+  delete(id) {
+    const customer = this.findById(id);
+    if (!customer) return false;
+    this.db.customers = this.db.customers.filter(
+      (customer) => customer.id !== id
+    );
+    return true;
+  }
+}
