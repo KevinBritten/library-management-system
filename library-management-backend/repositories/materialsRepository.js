02a0c8dc84d5d53c31e44a4471f9114b349dd6ba
Kevin Britten <kevinjbritten@gmail.com>
2025-01-23 14:39:26 -0500

    add materialsRepository

---
+++ library-management-backend/repositories/materialsRepository.js
@@ -0,0 +1,45 @@
+import MockDatabase from "../database/MockDatabase.js";
+
+class MaterialsRepository {
+  constructor() {
+    this.db = MockDatabase;
+  }
+
+  create(material) {
+    this.db.push(material);
+  }
+
+  findAll() {
+    return this.db.materials;
+  }
+
+  findById(id) {
+    return this.db.materials.find((material) => material.id == id) || null;
+  }
+
+  findByIsbn(isbn) {
+    return this.db.materials.find((material) => material.isbn == isbn) || null;
+  }
+
+  findAllByName(name) {
+    return this.db.materials.map((material) => material.name == name);
+  }
+
+  findAllByCategory(category) {
+    return this.db.materials.map((material) => material.category == category);
+  }
+
+  update(id, updateFields) {
+    const material = this.findById(id);
+    return material ? Object.assign(material, updateFields) : null;
+  }
+
+  delete(id) {
+    const material = this.findById(id);
+    if (!material) return false;
+    this.db.materials = this.db.materials.filter(
+      (material) => material.id !== id
+    );
+    return true;
+  }
+}
