1b60332c81f8cb01a4ade904d204654e1c150e37
Kevin Britten <kevinjbritten@gmail.com>
2025-01-25 15:52:47 -0500

    add initial pages

--- library-management-frontend/src/App.jsx
+++ library-management-frontend/src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+
+function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
