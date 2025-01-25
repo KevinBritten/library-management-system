05551ff62632bec9ad0ee4ee2c0072272cd39c4c
Kevin Britten <kevinjbritten@gmail.com>
2025-01-25 16:53:59 -0500

    add link to signup in home

--- library-management-frontend/src/pages/Home.jsx
+++ library-management-frontend/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <Link to={'/login'}>Login/Signup</Link>
+      <Link to={"/login"}>Login</Link>
+      <Link to={"/signup"}>Signup</Link>
     </div>
   );
 }
