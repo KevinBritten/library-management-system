f74d56db641b0f8294f30dd5835b58956bd6ef7b
Kevin Britten <kevinjbritten@gmail.com>
2025-01-25 10:55:40 -0500

    move user signupDate to constructor parameters

--- library-management-backend/models/User.js
+++ library-management-backend/models/User.js
@@ -1,13 +1,13 @@
 class User {
   static nextId = 1;
 
-  constructor(role, username, hashedPassword, salt) {
+  constructor(role, username, hashedPassword, salt, signupDate) {
     this.id = User.nextId++;
     this.role = role;
     this.username = username;
     this.hashedPassword = hashedPassword;
     this.salt = salt;
-    this.signupDate = Date.now();
+    this.signupDate = signupDate;
     this.token = "";
     this.tokenExpiration = null;
   }
