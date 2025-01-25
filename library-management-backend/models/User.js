907db3360999c7311951464d1a25e91cfc98b992
Kevin Britten <kevinjbritten@gmail.com>
2025-01-25 13:51:01 -0500

    remove salt and token from User model

--- library-management-backend/models/User.js
+++ library-management-backend/models/User.js
@@ -1,14 +1,13 @@
 class User {
   static nextId = 1;
 
-  constructor(role, username, hashedPassword, salt, signupDate) {
+  constructor(role, username, hashedPassword, signupDate) {
     this.id = User.nextId++;
     this.role = role;
     this.username = username;
     this.hashedPassword = hashedPassword;
-    this.salt = salt;
     this.signupDate = signupDate;
-    this.token = "";
-    this.tokenExpiration = null;
   }
 }
+
+export default User;
